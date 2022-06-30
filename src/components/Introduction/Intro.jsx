7db96d7a637c003ad182022d4092e4a9b540209e
Gyanendra Mohan <gypatel@deloitte.com>
2022-07-01 03:13:27 +0530

    added social media icons and resume

--- src/components/Introduction/Intro.jsx
+++ src/components/Introduction/Intro.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import "./Intro.css"
 import Profile from "../../img/Profile.png"
+import Resume from "./../../img/resume.pdf"
+
+import Github from "./../../img/github.png"
+import LinkedIn from "./../../img/linkedin.png"
+import Instagram from "./../../img/instagram.png"
+import fb1 from "./../../img/fb1.png"
 
 const Intro = () => {
     return (
@@ -22,10 +28,29 @@ const Intro = () => {
                             <div className="i-title-item">Gamer</div>
                         </div>
                     </div>
+
                     <div className="i-desc">
                         I am a Software Development Engineer Intern at HashedIn By Deloitte. I have done bTech
                         from UPES Dehradun, Uttarakhand India. I am From
                         Varanasi, the oldest living city.
+                    </div>
+
+                    <a href={Resume} download>
+                        <button className="resume-btn">Download CV</button>
+                    </a>
+                    <div className="i-icons">
+                        <a href='https://github.com/GyanendraMohan' target='_blank'>
+                            <img src={Github} alt="" />
+                        </a>
+                        <a href="https://www.linkedin.com/in/gyanendra-mohan-patel-599a491b5/" target='_blank'>
+                            <img src={LinkedIn} alt="" />
+                        </a>
+                        <a href="https://www.instagram.com/gyanendra_mohan/" target='_blank'>
+                            <img src={Instagram} alt="" />
+                        </a>
+                        <a href="https://www.facebook.com/gyanendra.mohan.79/" target='_blank'>
+                            <img src={fb1} alt="" />
+                        </a>
                     </div>
                 </div>
             </div>
