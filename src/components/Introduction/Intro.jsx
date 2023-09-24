9cee71b438767323eb3cceb1e10505454c40dc60
GyanendraMohan <gmpatel152@gmail.com>
2023-09-24 13:23:10 +0530

    intro changes and styles .

--- src/components/Introduction/Intro.jsx
+++ src/components/Introduction/Intro.jsx
@@ -22,20 +22,18 @@ const Intro = () => {
                     <div className="i-title">
                         <div className="i-title-wrapper">
                             <div className="i-title-item">Software Engineer</div>
-                            <div className="i-title-item">Web Developer</div>
-                            <div className="i-title-item">UI/UX Designer</div>
+                            <div className="i-title-item">Full Stack Developer</div>
                             <div className="i-title-item">Photographer</div>
                             <div className="i-title-item">Gamer</div>
                         </div>
                     </div>
 
                     <div className="i-desc">
-                        I am a Software Development Engineer Intern at HashedIn By Deloitte. I have done bTech
-                        from UPES Dehradun, Uttarakhand India. I am From
-                        Varanasi, the oldest living city.
+                        I am a Software Development Engineer - I at HashedIn By Deloitte. I have done B-Tech in computer Science
+                        from UPES Dehradun, Uttarakhand India. My Hometown is in <b>VARANASI.</b>
                     </div>
 
-                    <a href={Resume} download>
+                    <a href={Resume} download style={{ marginTop: "5px" }}>
                         <button className="resume-btn">Download CV</button>
                     </a>
                     <div className="i-icons">
